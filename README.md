0f634db8852be34667e0bcce9a76bb4e28ca8260
Ian Ozsvald <ian@ianozsvald.com>
2015-05-03 14:43:10 +0100

    first

---
+++ README.md
@@ -0,0 +1,12 @@
+Visualisation of @echen's #talkpay tweet/pay data scrape. Data from: 
+
+* https://twitter.com/echen/status/594353863374737409?s=03
+* https://docs.google.com/spreadsheets/d/1IK1an_x8buIveByENsAk6eww-mHIfZeYQxr7FBWPWz8/edit#gid=0
+* https://docs.google.com/spreadsheets/d/1IK1an_x8buIveByENsAk6eww-mHIfZeYQxr7FBWPWz8/htmlview?sle=true # not editable
+
+Run this script (note `normed` and `text_filters`), it'll generate an image and save it to a png.
+
+
+#![Summary of #talkpay pay data](talkpay_summary.png)
+
+#![Image of Yaktocat](https://octodex.github.com/images/yaktocat.png)
--- analysis1.py
+++ analysis1.py
@@ -4,13 +4,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+normed = True
+# filters can be an empty list, or positive items to extract from the data
+#text_filters = ['software', 'engineer', 'programmer']
+#text_filters = ['teach']  # teacher, teaching
+text_filters = []
+
 # load the data
 df = pd.read_csv("#TalkPay Tweets - Sheet1.csv")
 df['pay_'] = df['Yearly Pay'].map(lambda s: float(s.replace("$", "").replace(",", "")))
 df = df.ix[df['pay_'] > 0]  # filter at least $1000 (to remove guff tweets)
+
 # we could strip out data
-#df['sweng'] = df['Tweet'].map(lambda s: 'software' in s.lower() or 'engineer' in s.lower())
-#df = df[df['sweng']]
+def filter_keywords_present(s):
+    "True if any text_filters are present in lowercased s"
+    for fltr in text_filters:
+        if fltr in s.lower():
+            return True
+    return False
+
+if text_filters:
+    df['text_filter'] = df['Tweet'].map(filter_keywords_present)
+    print("Applying filter:", text_filters)
+    df = df[df['text_filter']]
 
 # set US locale
 locale.setlocale(locale.LC_ALL, 'en_US.utf8')
@@ -18,15 +34,17 @@ locale.setlocale(locale.LC_ALL, 'en_US.utf8')
 
 plt.figure(1)
 plt.clf()
-fontsize="medium"
-normed=True
+fontsize = "medium"
 x_max = 260001  # 280000
 step = 10000
 bins = np.arange(0, x_max, step)
 #x_labels = [locale.currency(x, grouping=True)[:-3]+"+" for x in np.arange(0, x_max, step)]
 x_labels = ["$"+str(x)[:-3]+"k+" for x in np.arange(0, x_max-1, step)]
 x_labels[0] = ">$0"
-res=plt.hist(df['pay_'], bins=bins, normed=normed, cumulative=normed)
+# Note that masked arrays aren't supportd by plt.hist, so we extract the raw
+# values
+pay = df['pay_'].values
+res=plt.hist(pay, bins=bins, normed=normed, cumulative=normed)
 ticks = plt.xticks(np.arange(0, x_max, step),
                    x_labels,
                    rotation=45,
@@ -43,8 +61,11 @@ if normed:
     for percentile in percentiles:
         plt.annotate("{:n}%".format(percentile*100), (10000, percentile+0.01))
 else:
-    plt.ylabel("Number of {} People".format(len(df)), fontsize=fontsize)
-plt.title("#TalkPay Tweets by @echen May 2015 (viz @IanOzsvald)")
+    plt.ylabel("Number of People (of {})".format(len(df)), fontsize=fontsize)
+title = "#TalkPay Tweets by @echen May 2015 (viz @IanOzsvald)"
+if text_filters:
+    title += "\nFiltered for any of: " + ", ".join(text_filters)
+plt.title(title)
 plt.xlim((0, x_max))
 plt.tight_layout()
 if normed:
